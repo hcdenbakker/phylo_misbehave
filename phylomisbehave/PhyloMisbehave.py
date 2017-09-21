1c82bc3fbda5370f670ef28f1608fdbf07d086a1
andrewjpage <andrewjpage@gmail.com>
2017-09-21 10:59:47 +0100

    fix indenting

--- phylomisbehave/PhyloMisbehave.py
+++ phylomisbehave/PhyloMisbehave.py
@@ -66,132 +66,126 @@ class PhyloMisbehave:
 		return None
 
 	def sliding_window(self, genome_size, positions):
-	    window_count = []
-	    window_start = []
-	    #self.logger.warning('starting sliding window')
-	    for i in range(0, genome_size - (self.sliding_window_size -1), self.sliding_window_size):
-	        window_start.append(i)
-	        window_count.append(len(set(range(i, i+(self.sliding_window_size -1)))& set(positions)))
-	    return window_count, window_start
+		window_count = []
+		window_start = []
+		#self.logger.warning('starting sliding window')
+		for i in range(0, genome_size - (self.sliding_window_size -1), self.sliding_window_size):
+			window_start.append(i)
+			window_count.append(len(set(range(i, i+(self.sliding_window_size -1)))& set(positions)))
+		return window_count, window_start
 	
 	
 	def findFeatures(self, gff, positions):
-	    geneswithSNPs=[]
-	    with open(gff, 'r') as featurefile:
-	        for line in featurefile:
-	            if (len(line.split('\t')) > 5) and line.split('\t')[2] == 'CDS':
-	                SNPS = len(set(positions) & set(range(int(line.split('\t')[3])-1, int(line.split('\t')[4]))))
-	                if SNPS > 0:
-	                    geneswithSNPs.append(line.split('\t')[8].split(';')[0].lstrip('ID='))
-	    return geneswithSNPs
+		geneswithSNPs=[]
+		with open(gff, 'r') as featurefile:
+			for line in featurefile:
+				if (len(line.split('\t')) > 5) and line.split('\t')[2] == 'CDS':
+					SNPS = len(set(positions) & set(range(int(line.split('\t')[3])-1, int(line.split('\t')[4]))))
+					if SNPS > 0:
+						geneswithSNPs.append(line.split('\t')[8].split(';')[0].lstrip('ID='))
+		return geneswithSNPs
 	
 	def featuresdict(self, gff):
-	    GeneDict={}
-	    with open(gff, 'r') as featurefile:
-	        for line in featurefile:
-	            if (len(line.split('\t')) > 5) and line.split('\t')[2] == 'CDS':
-	                GeneDict[line.split('\t')[8].split(';')[0].lstrip('ID=')] = (int(line.split('\t')[3])-1,\
-	                                                                            int(line.split('\t')[4])-1)
-	    return GeneDict
+		GeneDict={}
+		with open(gff, 'r') as featurefile:
+			for line in featurefile:
+				if (len(line.split('\t')) > 5) and line.split('\t')[2] == 'CDS':
+					GeneDict[line.split('\t')[8].split(';')[0].lstrip('ID=')] = (int(line.split('\t')[3])-1,\
+							int(line.split('\t')[4])-1)
+		return GeneDict
 	
 	
 	def run(self):
-	    alignment = findSNPs.parse_fasta(open(self.multifasta))
-	    ref = next(alignment)[1]
-	    n = len(ref)
-	    alignment = findSNPs.parse_fasta(open(self.multifasta))
+		alignment = findSNPs.parse_fasta(open(self.multifasta))
+		ref = next(alignment)[1]
+		n = len(ref)
+		alignment = findSNPs.parse_fasta(open(self.multifasta))
 		
-	    gff = self.gff_file
-	    varsites = findSNPs.findSNPs(ref, alignment)
-	    alignment = findSNPs.parse_fasta(open(self.multifasta))
+		gff = self.gff_file
+		varsites = findSNPs.findSNPs(ref, alignment)
+		alignment = findSNPs.parse_fasta(open(self.multifasta))
 		
-	    snps_ordered, id_snps = findSNPs.setupSNPdict(varsites, alignment)
-	    positions = [i for i in snps_ordered]
-	    window_count, window_start = self.sliding_window(n, positions)
-	    highsnps = [(window_start[i],j) for i,j in enumerate(window_count) if j > 5]
-	    filtered_positions = set(positions)
+		snps_ordered, id_snps = findSNPs.setupSNPdict(varsites, alignment)
+		positions = [i for i in snps_ordered]
+		window_count, window_start = self.sliding_window(n, positions)
+		highsnps = [(window_start[i],j) for i,j in enumerate(window_count) if j > 5]
+		filtered_positions = set(positions)
 		
-	    for i in highsnps:
-	        filtered_positions = filtered_positions - set(range(i[0], i[0]+1000))
-	    alignment = findSNPs.parse_fasta(open(self.multifasta))
-	    snps_ordered_filt, id_snps_filt = findSNPs.setupSNPdict(sorted(filtered_positions), alignment)
-	    with open(str(self.output_prefix)+'.unfiltered.fasta', 'w') as fp:
-	        for i in id_snps:
-	            if id_snps[i].count('-') > round(len(id_snps[i])*0.45):
+		for i in highsnps:
+			filtered_positions = filtered_positions - set(range(i[0], i[0]+1000))
+		alignment = findSNPs.parse_fasta(open(self.multifasta))
+		snps_ordered_filt, id_snps_filt = findSNPs.setupSNPdict(sorted(filtered_positions), alignment)
+		with open(str(self.output_prefix)+'.unfiltered.fasta', 'w') as fp:
+			for i in id_snps:
+				if id_snps[i].count('-') > round(len(id_snps[i])*0.45):
 					self.logger.warning(str(i)+' not in peak fasta!')
-	            else:
-	                fp.write('>'+ i +'\n')
-	                fp.write(''.join(id_snps[i])+'\n')
-	    subprocess.call([self.fasttree_exec+' -nt '+str(self.output_prefix)+'.unfiltered.fasta > '+str(self.output_prefix)+'.unfiltered.nwk']
-	                    , stdout=subprocess.DEVNULL, shell=True)
-	    with open(str(self.output_prefix)+'.peakfiltered.fasta', 'w') as fp:
-	        for i in id_snps_filt:
-	            if id_snps_filt[i].count('-') > round(len(id_snps_filt[i])*0.45):
+				else:
+					fp.write('>'+ i +'\n')
+					fp.write(''.join(id_snps[i])+'\n')
+		subprocess.call([self.fasttree_exec+' -nt '+str(self.output_prefix)+'.unfiltered.fasta > '+str(self.output_prefix)+'.unfiltered.nwk'], stdout=subprocess.DEVNULL, shell=True)
+		with open(str(self.output_prefix)+'.peakfiltered.fasta', 'w') as fp:
+			for i in id_snps_filt:
+				if id_snps_filt[i].count('-') > round(len(id_snps_filt[i])*0.45):
 					self.logger.warning(str(i)+' not in fasta!')
-	            else:
-	                fp.write('>'+ i +'\n')
-	                fp.write(''.join(id_snps_filt[i])+'\n')
-	    subprocess.call([self.fasttree_exec+' -nt '+str(self.output_prefix)+'.peakfiltered.fasta > '+str(self.output_prefix)+'.peakfiltered.nwk']
-	                    , stdout=subprocess.DEVNULL, shell=True)
-	    treeETE = Tree(str(self.output_prefix)+'.unfiltered.nwk')
-	    alignment = [(x,y) for (x,y) in findSNPs.parse_fasta(open(str(self.output_prefix)+'.unfiltered.fasta'))]
-	    nonhomoplasy, homoplasy = find_homoplasious_sites(alignment, treeETE)
-	    homoplasious_sites = [positions[x] for x in homoplasy]
-	    window_hom, window_start = self.sliding_window(n, homoplasious_sites)
-	    nonhomoplasious_sites = list(set(positions)-set(homoplasious_sites))
-	    alignment = findSNPs.parse_fasta(open(self.multifasta))
-	    snps_ordered_nonhom, id_snps_nonhom = findSNPs.setupSNPdict(nonhomoplasious_sites, alignment)
-	    with open(str(self.output_prefix)+'.nonhomoplasious.fasta', 'w') as fp:
-	        for i in id_snps_nonhom:
-	            if id_snps_nonhom[i].count('-') > round(len(id_snps_nonhom[i])*0.45):
+				else:
+					fp.write('>'+ i +'\n')
+					fp.write(''.join(id_snps_filt[i])+'\n')
+		subprocess.call([self.fasttree_exec+' -nt '+str(self.output_prefix)+'.peakfiltered.fasta > '+str(self.output_prefix)+'.peakfiltered.nwk'], stdout=subprocess.DEVNULL, shell=True)
+		treeETE = Tree(str(self.output_prefix)+'.unfiltered.nwk')
+		alignment = [(x,y) for (x,y) in findSNPs.parse_fasta(open(str(self.output_prefix)+'.unfiltered.fasta'))]
+		nonhomoplasy, homoplasy = find_homoplasious_sites(alignment, treeETE)
+		homoplasious_sites = [positions[x] for x in homoplasy]
+		window_hom, window_start = self.sliding_window(n, homoplasious_sites)
+		nonhomoplasious_sites = list(set(positions)-set(homoplasious_sites))
+		alignment = findSNPs.parse_fasta(open(self.multifasta))
+		snps_ordered_nonhom, id_snps_nonhom = findSNPs.setupSNPdict(nonhomoplasious_sites, alignment)
+		with open(str(self.output_prefix)+'.nonhomoplasious.fasta', 'w') as fp:
+			for i in id_snps_nonhom:
+				if id_snps_nonhom[i].count('-') > round(len(id_snps_nonhom[i])*0.45):
 					self.logger.warning(str(i)+' not in non homoplasious fasta!')
-	            else:
-	                fp.write('>'+ i +'\n')
-	                fp.write(''.join(id_snps_nonhom[i])+'\n')
-	    subprocess.call([self.fasttree_exec+' -nt '+str(self.output_prefix)+'.nonhomoplasious.fasta > '+str(self.output_prefix)+'.nonhomoplasious.nwk']
-	                    , stdout=subprocess.DEVNULL, shell=True)
-	
-	    peaks = set(positions) - set(filtered_positions)
-	    with open(str(self.output_prefix) + '_homoplasious_sites.txt', 'w') as f:
-	        for i in homoplasious_sites:
-	            f.write(str(i) +'\n')
-	    GenesWithclusteredSNPs = self.findFeatures(gff, homoplasious_sites)
-	    try:
-	        CodingGenes = SeqIO.parse(self.faa_file, 'fasta')
-	        featuredict = self.featuresdict(gff)
-	        PhageGenes = []
-	        for gene in CodingGenes:
-	            if str(gene.id) in GenesWithclusteredSNPs:
-	                self.logger.warning(gene.id)
-	                with open("temp.fasta", "w") as output_handle:
-	                    SeqIO.write(gene, output_handle, 'fasta')
+				else:
+					fp.write('>'+ i +'\n')
+					fp.write(''.join(id_snps_nonhom[i])+'\n')
+		subprocess.call([self.fasttree_exec+' -nt '+str(self.output_prefix)+'.nonhomoplasious.fasta > '+str(self.output_prefix)+'.nonhomoplasious.nwk'], stdout=subprocess.DEVNULL, shell=True)
+		
+		peaks = set(positions) - set(filtered_positions)
+		with open(str(self.output_prefix) + '_homoplasious_sites.txt', 'w') as f:
+			for i in homoplasious_sites:
+				f.write(str(i) +'\n')
+		GenesWithclusteredSNPs = self.findFeatures(gff, homoplasious_sites)
+		try:
+			CodingGenes = SeqIO.parse(self.faa_file, 'fasta')
+			featuredict = self.featuresdict(gff)
+			PhageGenes = []
+			for gene in CodingGenes:
+				if str(gene.id) in GenesWithclusteredSNPs:
+					self.logger.warning(gene.id)
+					with open("temp.fasta", "w") as output_handle:
+						SeqIO.write(gene, output_handle, 'fasta')
 						
-	                blastp_cline = NcbiblastpCommandline(query="temp.fasta", db=pkg_resources.resource_filename(__name__, 'databases/prophage_virus.db'), evalue=1.1E-11,
-	                                                     outfmt=5, out="test.xml", max_hsps=1, max_target_seqs=1, num_threads=4)
-	                blastp_cline()
-	                alignments = SearchIO.parse('test.xml', 'blast-xml')
-	                for alignment in alignments:
-	                    for hit in alignment.hits:
-	                        if hit.id.startswith('PHAGE') or hit.id.startswith('PROPHAGE'):
-	                            PhageGenes.append(gene.id)
+					blastp_cline = NcbiblastpCommandline(query="temp.fasta", db=pkg_resources.resource_filename(__name__, 'databases/prophage_virus.db'), evalue=1.1E-11,
+														 outfmt=5, out="test.xml", max_hsps=1, max_target_seqs=1, num_threads=4)
+					blastp_cline()
+					alignments = SearchIO.parse('test.xml', 'blast-xml')
+					for alignment in alignments:
+						for hit in alignment.hits:
+							if hit.id.startswith('PHAGE') or hit.id.startswith('PROPHAGE'):
+								PhageGenes.append(gene.id)
 								self.logger.warning(hit.id[0:75] + '...')
-	        phage_ranges = [featuredict[x] for x in PhageGenes]
-	        phagepositions = []
-	        for p in phage_ranges:
-	            phagepositions += list(range(p[0], p[1] + 1))
-	        variable_phage_positions = sorted(list(set(positions) & set(phagepositions)))
-	        with open(str(self.output_prefix) + '_phage_SNPs.txt', 'w') as f:
-	            for i in variable_phage_positions:
-	                f.write(str(i) + '\n')
-	        window_phage, window_start = self.sliding_window(n, variable_phage_positions)
-	        sns.plt.plot(window_start, window_phage, 'go')
-	
-	    except FileNotFoundError:
-	        self.logger.warning('Can not find AA gene file; will not perform phage related gene scan. Please provide .faa file if you\n '
-	              'want to perform this procedure. ')
-	
-	    sns.plt.plot(window_start, window_hom, 'ro')
-	    sns.plt.plot(window_start, window_count, color='b', alpha=0.7)
-	    plt.savefig(str(self.output_prefix) + 'SNP_homoplasy_density.pdf')
-
-
+			phage_ranges = [featuredict[x] for x in PhageGenes]
+			phagepositions = []
+			for p in phage_ranges:
+				phagepositions += list(range(p[0], p[1] + 1))
+			variable_phage_positions = sorted(list(set(positions) & set(phagepositions)))
+			with open(str(self.output_prefix) + '_phage_SNPs.txt', 'w') as f:
+				for i in variable_phage_positions:
+					f.write(str(i) + '\n')
+			window_phage, window_start = self.sliding_window(n, variable_phage_positions)
+			sns.plt.plot(window_start, window_phage, 'go')
+			
+		except FileNotFoundError:
+			self.logger.warning('Can not find AA gene file; will not perform phage related gene scan. Please provide .faa file if you\n want to perform this procedure. ')
+		
+		sns.plt.plot(window_start, window_hom, 'ro')
+		sns.plt.plot(window_start, window_count, color='b', alpha=0.7)
+		plt.savefig(str(self.output_prefix) + 'SNP_homoplasy_density.pdf')
